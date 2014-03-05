ddebe6b1cfe40ceecfb00025e9df5e50409fa09f
Thomas Cate <thomas.cate@rackspace.com>
2014-03-05 07:39:28 -0600

    moved forwarder fix to welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    if request.env["HTTP_X_FORWARDED_FOR"]
-      @remote_ip = request.env["HTTP_X_FORWARDED_FOR"]
-    else
-      @remote_ip = request.env['REMOTE_ADDR']
-    end
+    @remote_ip = request.env['HTTP_X_FORWARDED_FOR'].split(/ /)[0].chomp(",")
 
     render text: "#{@remote_ip}\n"
   end
