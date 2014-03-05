30a4236061f97d467501e7506a79b95219c53411
Thomas Cate <thomas.cate@rackspace.com>
2014-03-05 14:38:06 -0600

    typo fix

--- config/deploy/local.rb
+++ config/deploy/local.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
 set :branch, :master
-set :ssh_options, { :forward_agent => true, :StrictHostKeyChecking => false, :keys = ["/root/.ssh/id_rsa"] }
+set :ssh_options, { :forward_agent => true, :StrictHostKeyChecking => false, :keys => ["/root/.ssh/id_rsa"] }
 set :repo_url, 'https://github.com/thomascate/whoami'
 
 server '127.0.0.1', user: 'deploy', roles: 'app'
