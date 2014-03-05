51fac4137a8377df5a72be1d852dcf0b9f0bc9a5
Thomas Cate <thomas.cate@rackspace.com>
2014-03-05 08:01:40 -0600

    need full path for geoip to work

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,7 @@
 set :stage, :staging
 set :branch, :master
 set :ssh_options, { :forward_agent => true }
+set :repo_url, 'https://github.com/thomascate/whoami'
 
 #register_nodes("name:worker-st-01", ["app"])
 server '162.242.241.189', user: 'deploy', roles: 'app'
